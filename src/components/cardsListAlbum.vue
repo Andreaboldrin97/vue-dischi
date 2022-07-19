7bc29ddd03a5f08c68e39dba396c971865fbb968
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-19 16:08:32 +0200

    created cardsListAlbum.vue

---
+++ src/components/cardsListAlbum.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="container">
+        <div class="row row-cols-5">
+            <cardAlbum/>
+        </div>
+    </div>
+</template>
+
+<script>
+import cardAlbum from './cardAlbum.vue'
+export default {
+  components: {
+    cardAlbum,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+
+</style>

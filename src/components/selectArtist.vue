ce70ba7500cfebbc34b54155e8e5d45e6c092766
Andreaboldrin97 <boldrinandrea23@gmail.com>
2022-07-20 17:31:51 +0200

    created selectArtist.vue (bonus)

--- src/components/selectArtist.vue
+++ src/components/selectArtist.vue
@@ -1,21 +1,26 @@
 <template>
-   <select class="col-4 offset-4 rounded-3">
-          <option selected disabled value="">ARTISTA</option>
-          <option value="bonjovi">Bon Jovi</option>
-          <option value="queen">Queen</option>
-          <option value="sting">Sting</option>
-          <option value="stevegaddband">Steve Gadd Band</option>
-          <option value="ironmaiden">Iron Maiden</option>
-          <option value="ericclapton">Eric Clapton</option>
-          <option value="deeppurple">Deep Purple</option>
-          <option value="metallica">Metallica</option>
-          <option value="daveweckel">Dave Weckl</option>
-          <option value="michaeljakson">Micheal Jackson</option>
+   <select class="col-4 offset-4 rounded-3" v-model="selectedArtist" @change="$emit('select' , selectedArtist)">
+          <option selected value="">ARTISTA</option>
+          <option value="Bon Jovi">Bon Jovi</option>
+          <option value="Queen">Queen</option>
+          <option value="Sting">Sting</option>
+          <option value="Steve Gadd Band">Steve Gadd Band</option>
+          <option value="Iron Maiden">Iron Maiden</option>
+          <option value="Eric Clapton">Eric Clapton</option>
+          <option value="Deep Purple">Deep Purple</option>
+          <option value="Metallica">Metallica</option>
+          <option value="Dave Weckl<">Dave Weckl</option>
+          <option value="Micheal Jacjson">Micheal Jackson</option>
     </select>
 </template>
 
 <script>
 export default {
+     data: function(){
+        return{
+            selectedArtist : ''
+        }
+    }
 
 }
 </script>
